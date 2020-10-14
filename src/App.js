0c614b63d2a1acdbb49885339c3fab6b3b0adc2b
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-10-14 16:10:22 -0400

    added postNewUser axios request. added inputChange and formSubmit helper functions in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import * as yup from "yup";
+import OnboardForm from "./components/OnboardForm";
+// import schema from './components/FormSchema';
+
+const initFormValues = { name: "", email: "", password: "", tos: false };
+const initFormErrors = { name: "", email: "", password: "", tos: "" };
+const initUsers = [];
+const initDisabled = true;
 
 function App() {
+  const [users, setUsers] = useState(initUsers);
+  const [formValues, setFormValues] = useState(initFormValues);
+  const [formErrors, setFormErrors] = useState(initFormErrors);
+  const [disabled, setDisabled] = useState(initDisabled);
+
+  const postNewUser = (newUser) => {
+    axios
+      .post("https://reqres.in/api/users", newUser)
+      .then((result) => {
+        setUsers([result.data], ...users);
+        setFormValues(initFormValues);
+      })
+      .catch((err) => {
+        debugger;
+      });
+  };
+
+  const inputChange = (name, value) => {
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  const formSubmit = () => {
+    const newUser = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      tos: formValues.tos.trim()
+    }
+    postNewUser(newUser);
+  }
+
+  // adjust disabled everytime formValues changes.
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <OnboardForm />
     </div>
   );
 }
---
+++ src/components/OnboardForm.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const OnboardForm = (props) => {
+    // Name
+    // Email
+    // Password
+    // Terms of Service (checkbox)
+    // A Submit button to send our form data to the server.
+    return (
+        <form className='form container'>
+            <div className='form-group inputs'>
+                <input></input>
+            </div>
+        </form>
+    )
+}
+
+export default OnboardForm
